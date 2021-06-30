972e7fab5c23d905f82ebe93d996ae478adeb591
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-30 14:22:39 -0300

    Adição de lógica no controller

--- src/main/java/br/com/aula1/tt/codigomorse/controllers/CodigoMorseController.java
+++ src/main/java/br/com/aula1/tt/codigomorse/controllers/CodigoMorseController.java
@@ -1,5 +1,6 @@
 package br.com.aula1.tt.codigomorse.controllers;
 
+import br.com.aula1.tt.codigomorse.utils.CodigoMorse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ public class CodigoMorseController {
     @RequestMapping("/morse/{codigo}")
     public String codigoMorse(@PathVariable String codigo) {
         if(codigo.matches("[.-]{1,5}(?> [.-]{1,5})*(?>   [.-]{1,5}(?> [.-]{1,5})*)*")) {
-            return "Código Morse";
+            return codigo + " = " + CodigoMorse.codigoMorse(codigo);
         }
         return "Parâmetro inválido!";
     }
