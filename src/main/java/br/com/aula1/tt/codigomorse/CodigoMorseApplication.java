fc2221b1521c1f0b6444d5b0e12468a64951502b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-30 12:31:16 -0300

    Inicialização do projeto springboot

---
+++ src/main/java/br/com/aula1/tt/codigomorse/CodigoMorseApplication.java
@@ -0,0 +1,13 @@
+package br.com.aula1.tt.codigomorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoMorseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodigoMorseApplication.class, args);
+    }
+
+}
